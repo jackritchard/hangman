d8902cc20b5d3fee355dedda6e899a783152986c
jackritchard <96179700+jackritchard@users.noreply.github.com>
2022-07-05 00:32:52 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -18,6 +18,11 @@ let total = 0
 
 function randomNumber() {
     // random number between 0 and the length of the words array
+    //prevent the same word from being chosen twice in a row
+    let random = Math.floor(Math.random() * words.length);
+    if (random === total) {
+        random = randomNumber();
+    }
     return Math.floor(Math.random() * words.length);
 }
 
@@ -31,7 +36,13 @@ let words =
     "PEPPER",
     "KHAKI",
     "PANCAKE",
-    "PNEUMONIA"
+    "FOREIGN",
+    "DILEMMA",
+    "SCHOOL",
+    "SUNSET",
+    "CAT",
+
+    
 ];
 
 let definition = [
@@ -44,7 +55,16 @@ let definition = [
     "a pungent, hot-tasting powder prepared from dried and ground peppercorns, commonly used as a spice or condiment to flavor food.",
     "a textile fabric of a dull brownish-yellow color, in particular a strong cotton fabric used in military clothing.",
     "a thin, flat cake of batter, usually fried and turned in a pan. Pancakes are usually eaten with syrup or rolled up with a filling.",
-    "lung inflammation caused by bacterial or viral infection, in which the air sacs fill with pus and may become solid. Inflammation may affect both lungs ( double pneumonia ), one lung ( single pneumonia ), or only certain lobes ( lobar pneumonia )."
+    "of, from, in, or characteristic of a country or language other than one's own.",
+    "a dispute or disagreement between two or more people.",
+    "an institution for educating children.",
+    "the time in the evening when the sun disappears or daylight fades.",
+    "a small domesticated carnivorous mammal with soft fur, a short snout, and retractable claws. It is widely kept as a pet or for catching mice, and many breeds have been developed."
+
+
+
+
+    
 
 ]
 
@@ -73,14 +93,24 @@ function getWord (){
 getWord();
 
 buttons.forEach(function(button) {
+    //while held down, add clicked class
+    button.addEventListener('mousedown', function() {
+        button.classList.add('clicked');
+    })
+    //while not held down, remove clicked class
+    button.addEventListener('mouseup', function() {
+        button.classList.remove('clicked');
+    }
+    )
 
     button.addEventListener('click', function a(button) {
 
-        button.target.classList.add('disabled');
+        
 
         for (let i = 0; i < letters.length; i++) {
             if (letters[i].innerHTML === button.target.innerHTML) {
                 letters[i].classList.add('revealed');
+                button.target.classList.add('disabledtrue');
                  if (document.querySelectorAll('.revealed').length === letters.length) {
                     winstreak += 1
                     streak.textContent = `Win Streak: ${winstreak}`
@@ -88,6 +118,7 @@ buttons.forEach(function(button) {
                     total += 1;}}}    
 
         if (!words[word].includes(button.target.innerHTML)) {
+            button.target.classList.add('disabled');
             for (let i = 0; i < theman.length; i++) {
                 if (!theman[i].classList.contains('displayman')) {
                     theman[i].classList.add('displayman');
@@ -109,7 +140,8 @@ hint.addEventListener('click', function b() {
 
 function gameovershow() {
     theman.forEach(line => { line.classList.remove('displayman')})
-    buttons.forEach(button => { button.classList.remove('disabled')})
+    buttons.forEach(button => { button.classList.remove('disabled') 
+        button.classList.remove('disabledtrue')})
     winner.classList.remove('winnerclass')
     gameover.classList.remove('gameovershow')
     bottom.classList.remove('gameoverhide')
@@ -138,17 +170,3 @@ function gameoverhide(x) {
     h1.classList.add('gameoverhide')
     buttons.forEach(button => { button.classList.add('disabled')})
     setTimeout(gameovershow, 2100)}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
